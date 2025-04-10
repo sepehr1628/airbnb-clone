ee7201acf3b72dfc354f661fddb77b36f6aac1bf
Sepehr Mohamadpor <sepehr1628@gmail.com>
2025-04-11 02:42:03 +0330

    feat: modal created and styled.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import { Nunito } from "next/font/google";
 import Navbar from "./_components/Navbar";
+import Modal from "./_components/Modal";
 
 export const metadata: Metadata = {
   title: "Airbnb clone",
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal actionLabel="Submit" title="Hello World!" isOpen />
         <Navbar />
         {children}
       </body>
